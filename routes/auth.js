6cc4c094054466e0b435a1e1d35ff22d8a2e0643
melek <melekhedhili7@gmail.com>
2022-12-12 22:08:34 +0100

    commit

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const passport = require("passport");
+router.get("/login/failed", (req, res) => {
+  res.status(401).json({
+    error: true,
+    message: "Login failure",
+  });
+});
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: "Successfully Loggedin",
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ error: true, message: "Not Authorized" });
+  }
+});
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
+);
+router.get("/google", passport.authenticate("google", ["profile", "email"]));
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(process.env.CLIENT_URL);
+});
+module.exports = router;
